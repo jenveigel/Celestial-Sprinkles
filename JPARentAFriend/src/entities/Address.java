fd4df5652cceca8023418c66a5f66df7f907e905
Jacob Simms <simmsjt@Jacobs-MacBook-Pro.local>
2017-11-29 16:11:54 -0700

    created Address entity

---
+++ JPARentAFriend/src/entities/Address.java
@@ -0,0 +1,33 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", address=" + address + "]";
+	}
+	
+}
