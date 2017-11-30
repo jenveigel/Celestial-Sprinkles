6a8321560eb9ae3ff82b16fbd608ed0c52f19b02
David Stumpe <dstumpe264@gmail.com>
2017-11-30 14:23:19 -0700

    tested create with create event

--- JPARentAFriend/src/client/eventDAOtest.java
+++ JPARentAFriend/src/client/eventDAOtest.java
@@ -1,5 +1,35 @@
 package client;
 
-public class eventDAOtest {
+import java.sql.Date;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import data.EventDAO;
+import entities.Address;
+import entities.Event;
+import entities.User;
+
+public class eventDAOtest {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("RentAFriend");
+		EntityManager em = emf.createEntityManager();
+		
+		EventDAO dao = new EventDAO();
+		Event event = new Event();
+		
+		event.setActivity("Drinking");
+		Address add = em.find(Address.class, 1);
+		event.setAddress(add);
+		Date date = new Date(2017, 03, 12);
+		event.setDateTime(date);
+		User user = new User();
+		event.setOwnerId(em.find(User.class, 1));
+		dao.create(event);
+		em.close();
+		emf.close();
+//		System.out.println(em.find(Address.class, 1));
+//		System.out.println(user.getUserName());
+	}
 }
