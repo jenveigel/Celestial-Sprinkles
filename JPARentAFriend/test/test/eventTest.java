c2e1fbf84c8407436e52d5d4744f37a217d6239e
invictvs <david.chirdon@gmail.com>
2017-11-30 10:19:44 -0700

    added eventTest, Event create, destroy methods, Address mapping to event, removed id field from Event constructor

---
+++ JPARentAFriend/test/test/eventTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import entities.Event;
+
+public class eventTest {
+	
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	private Event event;
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("RentAFriend");
+		EntityManager em = emf.createEntityManager();
+		
+		Event event = em.find(Event.class, 1);
+		System.out.println(event.getAddress().getCity());
+
+		em.close();
+		emf.close();
+	}
+
+}
