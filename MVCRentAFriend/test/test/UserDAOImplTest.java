789a0a89678304db0038e844199c53d9cd9cb33c
falcon <aaron.skilldistill@gmail.com>
2017-12-04 14:35:53 -0700

    fix userDAOImplTest

--- MVCRentAFriend/test/test/UserDAOImplTest.java
+++ MVCRentAFriend/test/test/UserDAOImplTest.java
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-//package test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.EntityManagerFactory;
-//import javax.persistence.Persistence;
-//
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import data.UserDAO;
-//import data.UserDAOImpl;
-//
-//public class UserDAOImplTest {
-//	private EntityManagerFactory emf;
-//	private EntityManager em;
-//	private UserDAO userDao;
-//	
-//	
-//	@Before
-//	public void setUp() throws Exception {
-//		this.userDao  = new UserDAOImpl();
-//		this.emf = Persistence.createEntityManagerFactory("RentAFriend");
-//		this.em = emf.createEntityManager();
-//	}
-//	
-//	@After
-//	public void tearDown() throws Exception{
-//		em.close();
-//		emf.close();
-//	}
-//	
-//	@Test
-//	public void test() {
-//		boolean pass = true;
-//		assertEquals(pass, true);
-//	}
-//	
-//	@Test
-//	public void test_create_user() {
-//		userDao.createUser("jake", "theSnake", "jake", "simms");
-//		assertEquals("jake", userDao.getUserById(3).getUserName());
-//		assertEquals("theSnake", userDao.getUserById(3).getPassword());
-//	}
-//}
-//
-=======
 package test;
 
 import static org.junit.Assert.assertEquals;
@@ -89,4 +39,3 @@ public class UserDAOImplTest {
 	}
 
 }
->>>>>>> 97b4194ae3947b5c66de0e4bcd512c6d11af094a
