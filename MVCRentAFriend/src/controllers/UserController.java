7c4ffd8814520299bb47fda9c6d238c69511b196
David Stumpe <dstumpe264@gmail.com>
2017-12-11 13:03:25 -0700

    n

--- MVCRentAFriend/src/controllers/UserController.java
+++ MVCRentAFriend/src/controllers/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 import data.UserDAO;
 import entities.Event;
 import entities.User;
-//why
+//why i dont know
 @Controller
 public class UserController {
 
